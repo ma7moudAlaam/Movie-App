51832860fb9162a78338f66dff852d4ad52f0d16
Ali Elsheshtawy <alielsheshtawy0@gmail.com>
2023-11-10 17:53:09 +0200

    finished watch list UI

--- lib/pages/watchlist/watchlist_view.dart
+++ lib/pages/watchlist/watchlist_view.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:movie/pages/watchlist/watchlist_item.dart';
 
 class WatchListView extends StatelessWidget {
   const WatchListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("WatchListView"),);
+    ThemeData theme = Theme.of(context);
+    var mediaQuery = MediaQuery.of(context).size;
+    return Padding(
+      padding: EdgeInsets.only(
+        right: mediaQuery.width * 0.028,
+        left: mediaQuery.width * 0.028,
+        top: mediaQuery.height * 0.1,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Watchlist",
+            style: theme.textTheme.titleLarge,
+          ),
+          SizedBox(
+            height: mediaQuery.height * 0.028,
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.zero,
+              itemBuilder: (context, build) => const WatchListItem(),
+              itemCount: 20,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
